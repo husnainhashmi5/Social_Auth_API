600ba58423840fa082691ac003c627a8e4c68b0d
husnainhashmi5 <husnain4work@gmail.com>
2025-01-08 19:44:19 +0500

    modify code

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
+
+    def ready(self):
+        from allauth.socialaccount.models import SocialAccount
+        def patched_socialaccount_str(self):
+            return str(self.user)
+        SocialAccount.__str__ = patched_socialaccount_str
